55b31fdfc766e40da0f1888ca7cdbe91c1a22739
baizai <745952066@qq.com>
2017-07-25 17:55:38 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/aa/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package com.example.aa.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by aa on 2017/7/25.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
